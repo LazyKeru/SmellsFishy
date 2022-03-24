2e42ee07563f020eca18caba8cfd4413e2f33e26
EloiGG <81160762+EloiGG@users.noreply.github.com>
2022-03-24 20:28:28 +0100

    feat: added json loading fonctionnalities

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,23 @@
 #include "../header/regex/regex.hpp"
 #include "../header/files/readDir.hpp"
 #include <vector>
+#include "../header/files/json.hpp"
+#include "../header/struct/rule.hpp"
+#include "../header/struct/secretsPerFile.hpp"
 
 int main(int argc, char const *argv[])
 {
+    JSON rgx_json("C:/Users/Eloi/Desktop/Projets/SmellsFishy/SmellsFishy/resources/rgx_list.json");
+   
+    std::vector<std::shared_ptr<Rule>> rules;
+
+    rules.push_back(Rule::getRuleSharedPtr(rgx_json.getRuleFromDescription("PGP private key")));
+    rules.push_back(Rule::getRuleSharedPtr(rgx_json.getRuleFromDescription("Shopify shared secret")));
+    rules.push_back(Rule::getRuleSharedPtr(rgx_json.getRuleFromDescription("Slack token")));
+    rules.push_back(Rule::getRuleSharedPtr(rgx_json.getRuleFromDescription("Discord client ID")));
+
+    auto files = Files::readDirRecursive("C:/Users/Eloi/Desktop/Projets/SmellsFishy");
+    std::vector<SecretsPerFile> allSecrets(files.size());
+
     return 0;
 }
--- src/struct/rule.cpp
+++ src/struct/rule.cpp
@@ -1,12 +1,13 @@
 #include "../../header/struct/rule.hpp"
+#include "../../header/util/util.hpp"
 
 std::shared_ptr<Rule> Rule::getRuleSharedPtr(const Rule &r)
 {
     return std::make_shared<Rule>(r);
 }
 
-Rule::Rule(const std::string& name, const std::string& description, const std::string& regex, double maxEntropy, double minEntropy) :
-    name(name), description(description), regex(regex), maxEntropy(maxEntropy), minEntropy(minEntropy)
+Rule::Rule(const std::string& _name, const std::string& _description, const std::string& _regex, double maxEntropy, double minEntropy) :
+    name(_name), description(_description), regex(_regex), maxEntropy(maxEntropy), minEntropy(minEntropy)
 {
 }
 
