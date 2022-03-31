36cd939c5a568c98819ad5b0309bb6eaeeac85af
LazyKeru <killian.allaire@hotmail.com>
2022-03-31 20:03:38 +0200

    refactor: now includes entropy when adding all the rules
    refactor: now includes entropy when adding all the rules

--- src/files/json.cpp
+++ src/files/json.cpp
@@ -11,11 +11,20 @@ std::vector<Rule> JSON::getAllRulesFromJSON()
     std::vector<Rule> vec;
     vec.reserve(json["rules"].size());
 
-    for (const auto &r : json["rules"])
+    for (const auto &r : json["rules"]){
+        double maxEntropy = -1;
+        if(r.contains("maxEntropy")){
+            maxEntropy = r["maxEntropy"];
+        }
+        double minEntropy = -1;
+        if(r.contains("minEntropy")){
+            minEntropy = r["minEntropy"];
+        }
         vec.push_back(Rule(
             std::string(r["id"]), std::string(r["description"]),
-            std::string(r["regex"]), not_a_number
+            std::string(r["regex"]), maxEntropy, minEntropy
         ));
+    }
 
     return vec;
 }
@@ -26,7 +35,19 @@ Rule JSON::getRuleFromDescription(const std::string &description)
     std::string name(rule["id"]);
     std::string desc(rule["description"]);
     std::string reg(rule["regex"]);
-    Rule r(name, desc, reg, not_a_number, not_a_number);
+    double maxEntropy;
+    if(rule["maxEntropy"]){
+        maxEntropy = rule["maxEntropy"];
+    }else{
+        maxEntropy = not_a_number;
+    }
+    double minEntropy;
+    if(rule["minEntropy"]){
+        minEntropy = rule["minEntropy"];
+    }else{
+        minEntropy = not_a_number;
+    }
+    Rule r(name, desc, reg, maxEntropy, minEntropy);
     // Rule r(std::string(rule["id"]), std::string(rule["description"]), std::string(rule["regex"]),
     //        not_a_number, not_a_number);
     return r;
